b4e82dd6f7ee8b0b1794b8fca0d31f6e0e7c73a7
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-10 14:21:55 +0200

    basic view

--- src/blogclient/src/App.js
+++ src/blogclient/src/App.js
@@ -1,18 +1,88 @@
 import React from 'react';
 import './App.css';
 import post from "./post";
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PhoneIcon from '@material-ui/icons/Phone';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ViewStreamIcon from '@material-ui/icons/ViewStream';
+import AddIcon from '@material-ui/icons/Add';
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
+  return (
+      <Typography
+          component="div"
+          role="tabpanel"
+          hidden={value !== index}
+          id={`scrollable-force-tabpanel-${index}`}
+          aria-labelledby={`scrollable-force-tab-${index}`}
+          {...other}
+      >
+        {value === index && <Box p={3}>{children}</Box>}
+      </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    'aria-controls': `scrollable-force-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 function App() {
 
 let lista = [];
 lista.push(new post(1,"master","eka"));
 lista.push(new post(2, "kalle", "toka"));
 lista.push(new post(3, "master", "kolmas"));
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div className="App">
-
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="on"
+            indicatorColor="primary"
+            textColor="primary"
+            aria-label="scrollable force tabs example"
+        >
+          <Tab label="Show posts" icon={<ViewStreamIcon />} {...a11yProps(0)} />
+          <Tab label="Add new post" icon={<AddIcon />} {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
     </div>
   );
 }
