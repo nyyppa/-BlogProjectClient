9e718c1f4e654c3a3c2c003ee168f3ec41861093
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-04-08 16:38:20 +0300

    clean some imports

--- src/blogclient/src/CommentList.js
+++ src/blogclient/src/CommentList.js
@@ -1,10 +1,5 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
-import AutoSizer from "react-virtualized-auto-sizer";
-import {FixedSizeList} from "react-window";
-import ListItem from "@material-ui/core/ListItem";
-import Box from "@material-ui/core/Box";
-import ListItemText from "@material-ui/core/ListItemText";
 const useStyles2 = makeStyles(theme => ({
     root: {
         width: '100%',
